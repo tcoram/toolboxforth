b05600c038c3c096e17180a9fb5c070d48b3ec06
todd <todd@ThinkPad-X1-Carbon11.mynetworksettings.com>
2023-10-27 19:51:55 -0400

    improvements but still aborts when complete

--- tasks.f
+++ tasks.f
@@ -1,9 +1,15 @@
 \ Nothing formal yet.. just a playground to play with a tasking/coroutine concept.
 \
 
-\ Three words define the whole coroutine capability:
-\  make-task, start-task and yield.
+\ Always Task 0 :  the console and main interpreter.
 \
+uram constant tsk0
+
+\ We always "yield" control back to Task 0, which is resposible for handling
+\ dispatching..
+\
+: (yield) uram! ;
+: yield tsk0 uram! ;
 
 \ make a task.. rstack size datastack size number of cell and a variable for task ref
 \ Are loaded into RAM by allocating the proper amount *right after* the variable
@@ -19,20 +25,21 @@
     r@ 4 + ! 
     r@ 5 + ! r> drop  ;
 
+: end-task ( task -)
+    yield
+    >r
+    -1 2 r@ + !			\ reset data stack
+    4 r@ + @ 5 r@ +  @ + 3 r> + ! ;	\ reset return stack
+
+
 \ For start-task,  uram! *must* be called directly. This just compiles it into your task
 \ The black magic for tasking relies on the return stack...
 \
 : start-task ( tsk - )
+    dup end-task
     [compile] uram! ; immediate
 
-\ Always Task 0 :  the console and main interpreter.
-\
-uram constant tsk0
 
-\ We always "yield" control back to Task 0, which is resposible for handling
-\ dispatching..
-\
-: yield tsk0 uram! ;
 
 \ Take a look at was made
 : .task ( addr - )
@@ -50,12 +57,13 @@ uram constant tsk0
 \ You *must* make the task right after declaring its variable...
 \
 variable tsk1  50 50 10 tsk1 make-task
-
 tsk1 .task
 
 variable tsk2 50 50 10 tsk2 make-task 
-
 tsk2 .task
+
+variable tsk3 10 10 2 tsk3 make-task 
+tsk3 .task
 
 : sub-a 15 1 do i . cr yield  loop  ;
 : co-a  start-task yield sub-a ;
@@ -73,11 +81,58 @@ tsk2 .task
 : (wait) ( ms - ) 0 0 rot poll drop ;
 : wait ( ms - )  0 do 1 (wait) yield loop ;
     
-: co-c  start-task yield 5 1 do i . cr 500 wait loop  ;
-: co-d  start-task 5 1 do i 64 + emit cr 500 wait loop ;
+: co-c  start-task yield 10 1 do i . cr 500 wait loop  ;
+: co-d  start-task yield 10 1 do i 64 + emit cr 500 wait loop ;
+: co-e  start-task yield begin [char] . emit 100 wait again  ;
 
-: docd
+: docde
     tsk1 co-c
     tsk2 co-d
+    tsk3 co-e
+    begin  tsk1 (yield) tsk2 (yield) tsk3 (yield) again ;
+
+
+: test-do-no-task  10000000 0 do i drop loop ;
+: test-do  10000000 0 do i drop yield loop ;
+: test-for  10000000 for r@ drop yield next ;
+: test-begin-until 10000000  A ! begin -1 A +! A @ yield 0= until ;
+: test-begin-again 10000000  A ! begin -1 A +! A @ 0= if exit then yield again ;
+: test-begin-until-stack 10000000 begin 1- dup yield 0= until drop ;
+: tail-call 1- dup 0> if yield tail-call then drop ;
+: test-tail-call 10000000 tail-call ;
+
+: time-test
+   ['] test-do-no-task time-it
+   ['] test-do time-it
+    tsk1 start-task 
+    ['] test-do time-it ;
+: time-test-task
+    time-test begin tsk1 (yield) again ;
+
+: loop-tests1
+    start-task
+    yield
+    ." testing for .. next: "
+    ['] test-for time-it 
+    ." testing tail call: "
+    ['] test-tail-call time-it
+    ." testing do .. loop: "
+    ['] test-do time-it ;
+
+: loop-tests2
+    start-task
+    yield
+    ." testing begin .. until: "
+    ['] test-begin-until time-it 
+    ." testing begin .. again: "
+    ['] test-begin-again time-it  
+    ." testing begin .. until-stack: "
+    ['] test-begin-until-stack time-it ;
+
+: loop-tests-tasks
+    ." testing 10 million iteration loops" cr
+    tsk1 loop-tests1
+    tsk2 loop-tests2
     begin  tsk1 (yield) tsk2 (yield) again ;
+
 
